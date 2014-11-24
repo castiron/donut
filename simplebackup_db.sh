802d56d9cc3bacca1982b97b1d17c24f74adb305
Clark Burns <clark@castironcoding.com>
2014-11-24 13:49:50 -0800

    [BUGFIX] Fixes issue sourcing configuration;

--- simplebackup_db.sh
+++ simplebackup_db.sh
@@ -2,30 +2,28 @@
 # Author - clark@castironcoding.com
 # Purpose - Generates daily backup with retention for 1 week. Eventually smooch will replace this.
 
-dateToday=$(date +"%m_%d_%Y")
-dateDrop=$(date +%m_%d_%Y -d "1 week ago")
-
 scriptFilePath=$(readlink -f $0)
 scriptDir=$(dirname ${scriptFilePath})
 
-configFile="${scriptDir}/settings.cfg"
-
-
+dateToday=$(date +"%m_%d_%Y")
+dateDrop=$(date +%m_%d_%Y -d "1 week ago")
 backupFile="${scriptDir}/${backupFileName}_${dateToday}.sql"
 dropFile="${scriptDir}/${backupFileName}_${dateDrop}.sql"
 logFile="${scriptDir}/backup.log"
-
+configFile="${scriptDir}/settings.cfg"
 
 if [ -f ${logFile} ];
 then
     touch ${logFile}
 fi
 
-if [ -f ! ${configFile} ];
+if [ ! -f ${configFile} ];
 then
     echo '[ERROR!]' >> ${logFile}
     echo 'The file settings.cfg not found!' >> ${logFile}
     exit 1
+else
+    source ${configFile}
 fi
 
 echo "[STARTING BACKUP]" >> ${logFile}
