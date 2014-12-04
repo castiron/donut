31191af96edafb9da618f11d821f8c99d3026d7c
Clark Burns <clark@castironcoding.com>
2014-12-03 18:22:33 -0800

    [FEATURE] Ondemand backups;

--- simplebackup_db.sh
+++ simplebackup_db.sh
@@ -27,6 +27,24 @@ dateDrop=$(date +%m_%d_%Y -d "1 week ago")
 backupFile="${scriptDir}/${backupFileName}_${dateToday}.sql"
 dropFile="${scriptDir}/${backupFileName}_${dateDrop}.sql"
 
+if [ $# -gt 0 ]
+then
+    if [[ ${1} == 'ondemand' ]]
+    then
+        echo "Creating ondemand .sql dump of: ${dbName}"
+        onDemandBackupFile="${scriptDir}/${backupFileName}_ONDEMAND_${dateToday}.sql"
+        echo "Filename: ${onDemandBackupFile}"
+        mysqldump -h${dbHost} -u${dbUser} -p${dbPass} ${dbName} > ${onDemandBackupFile}
+        echo "Tada. All done."
+        exit 0
+    else
+        echo "Gibberish."
+        echo "Pass 'ondemand' as argument for one off .sql dump."
+        exit 1
+    fi
+fi
+
+
 echo "[STARTING BACKUP]" >> ${logFile}
 echo "Backup file: ${backupFile}" >> ${logFile}
 echo "Drop file: ${dropFile}" >> ${logFile}
